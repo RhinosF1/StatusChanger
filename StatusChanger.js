e014312c35e96c95972e8cb43317e10296630b7a
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-03-03 12:57:43 +0000

    Bug fix
    Bug fixes from test

--- StatusChanger.js
+++ StatusChanger.js
@@ -13,7 +13,7 @@ addOnloadHook(function (){
   }
  
   if (typeof(statusChangerConfig.statusList) == 'undefined') {
-      statusChangerConfig.statusList = [ 'online', 'offline', 'sleeping', 'around(A)', 'around(R)', 'BOW(A)', 'BOW(R) ];
+      statusChangerConfig.statusList = [ 'online', 'offline', 'sleeping', 'around(A)', 'around(R)', 'BOW(A)', 'BOW(R)' ];
   }
  
   if (typeof(statusChangerConfig.statusPage) == 'undefined') {
