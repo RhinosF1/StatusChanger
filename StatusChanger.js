c59545d22044866406e0686b5beba3ab3543d299
awesomelyidiot <46594821+awesomelyidiot@users.noreply.github.com>
2019-04-05 10:54:18 +0545

    fixed indentation and formatting

--- StatusChanger.js
+++ StatusChanger.js
@@ -6,33 +6,38 @@
 //     Modified by APerson for compatibility with {{UserStatus}}
 //     Modified by RhinosF1 for compatibili with his script.
 // compatible with {{User:RhinosF1/Template/StatusMonitor}}
-addOnloadHook(function (){
+addOnloadHook(function () {
   //Check if the config is defined
-  if (typeof(statusChangerConfig) == 'undefined') {
+  if (typeof (statusChangerConfig) == 'undefined') {
     statusChangerConfig = {}
   }
- var setMessage = function (stat){
-var message=stat;
-if (stat === "sleeping") {  message = "asleep" ;
-}
- else if(stat === "BOW(A)") {  message = "busy dealing with something on-wiki" ; }
-  else if (stat === "BOW(R)") {  message = "very busy dealing with something on-wiki" ; }
- else if (stat === "around(A)") {  message = "around" ;}
-  else if (stat === "around(R)") {  message = "around";}
-return message;
-}
-  if (typeof(statusChangerConfig.statusList) == 'undefined') {
-      statusChangerConfig.statusList = [ 'online', 'offline', 'sleeping', 'around(A)', 'around(R)', 'BOW(A)', 'BOW(R)' ];
+  var setMessage = function (stat) {
+    var message = stat;
+    if (stat === "sleeping") {
+      message = "asleep";
+    } else if (stat === "BOW(A)") {
+      message = "busy dealing with something on-wiki";
+    } else if (stat === "BOW(R)") {
+      message = "very busy dealing with something on-wiki";
+    } else if (stat === "around(A)") {
+      message = "around";
+    } else if (stat === "around(R)") {
+      message = "around";
+    }
+    return message;
   }
- 
-  if (typeof(statusChangerConfig.statusPage) == 'undefined') {
-      statusChangerConfig.statusPage = 'User:' + wgUserName + '/Status';
+  if (typeof (statusChangerConfig.statusList) == 'undefined') {
+    statusChangerConfig.statusList = ['online', 'offline', 'sleeping', 'around(A)', 'around(R)', 'BOW(A)', 'BOW(R)'];
   }
- var msg;
+
+  if (typeof (statusChangerConfig.statusPage) == 'undefined') {
+    statusChangerConfig.statusPage = 'User:' + wgUserName + '/Status';
+  }
+  var msg;
   //Add the links
-  for (var i=0; i<statusChangerConfig.statusList.length; i++) {
+  for (var i = 0; i < statusChangerConfig.statusList.length; i++) {
     var stat = statusChangerConfig.statusList[i];
-msg=setMessage (stat);
+    msg = setMessage(stat);
     addPortletLink(
       "p-personal", //target tab - personal links
       wgServer + wgScript + "?title=" + statusChangerConfig.statusPage + "&action=submit&newstatus=" + stat, //link URL
@@ -42,21 +47,22 @@ msg=setMessage (stat);
       "", //???
       document.getElementById("pt-logout")); //add before logout button
   }
- 
+
   if (location.href.indexOf("&action=submit&newstatus=") == -1) return; //Are we here to auto-edit the status?
   //Get new status
   statusRegExp = /&action=submit&newstatus=(.*)/;
   var status = statusRegExp.exec(location.href)[1];
   //Modify the form
   document.getElementById('wpTextbox1').value = status;
-msg=setMessage (status);
-  if (status == "sleep")
-  { status = "sleeping"; }
-  
-document.getElementById('wpSummary').value = wgUserName + " is now " + msg +".";
+  msg = setMessage(status);
+  if (status == "sleep") {
+    status = "sleeping";
+  }
+
+  document.getElementById('wpSummary').value = wgUserName + " is now " + msg + ".";
   document.getElementById('wpMinoredit').checked = true;
   //Submit it!
   document.getElementById('editform').submit();
 });
- 
+
 //[[Category:Wikipedia scripts|statusChanger]]
