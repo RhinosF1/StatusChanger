c2182b6f5105934962f44d5750959030e2d5221f
hyuinter <46594821+awesomelyidiot@users.noreply.github.com>
2019-04-17 10:45:35 +0545

    addOnloadHook, wgUserName, wgServer, wgScript, and  addPortletLink are deprecated.

--- StatusChanger.js
+++ StatusChanger.js
@@ -6,7 +6,10 @@
 //     Modified by APerson for compatibility with {{UserStatus}}
 //     Modified by RhinosF1 for compatibility with his script.
 // compatible with {{User:RhinosF1/Template/StatusMonitor}}
-addOnloadHook(function() {
+$(function() {
+  var wgUserName = mw.config.get("wgUserName");
+  var wgServer = mw.config.get("wgServer");
+  var wgScript = mw.config.get("wgScript");
   //Check if the config is defined
   if (typeof(statusChangerConfig) == 'undefined') {
     statusChangerConfig = {};
@@ -48,7 +51,7 @@ addOnloadHook(function() {
   for (var i = 0; i < statusChangerConfig.statusList.length; i++) {
     var stat = statusChangerConfig.statusList[i];
     msg = setMessage(stat);
-    addPortletLink(
+    mw.util.addPortletLink(
       "p-personal", //target tab - personal links
       wgServer + wgScript + "?title=" + statusChangerConfig.statusPage + "&action=submit&newstatus=" + stat, //link URL
       stat, //link text
